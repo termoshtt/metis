ef7d9bb7beabb3645aba89786019c652277799eb
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-10-12 01:41:20 +0900

    Parse header

--- metis/src/io.rs
+++ metis/src/io.rs
@@ -19,8 +19,141 @@ pub enum InvalidGraphFileError {
     },
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Format {
+    has_vertex_size: bool,
+    has_vertex_weight: bool,
+    has_edge_weight: bool,
+}
+
+impl Format {
+    fn new(fmt: &str) -> Self {
+        assert_eq!(fmt.len(), 3);
+        let byte2bool = |byte| match byte {
+            b'1' => true,
+            b'0' => false,
+            _ => panic!("Invalid format specification in Graph file header: {}", fmt),
+        };
+        let bytes = fmt.as_bytes();
+        Format {
+            has_vertex_size: byte2bool(bytes[0]),
+            has_vertex_weight: byte2bool(bytes[1]),
+            has_edge_weight: byte2bool(bytes[2]),
+        }
+    }
+}
+
+impl Default for Format {
+    fn default() -> Self {
+        Format {
+            has_edge_weight: true,
+            has_vertex_weight: true,
+            has_vertex_size: true,
+        }
+    }
+}
+
+/// Header of METIS Graph file
+struct Header {
+    /// Number of vertices
+    num_vertices: usize,
+    /// Number of edges
+    num_edges: usize,
+    fmt: Format,
+    /// Number of vertex weights associated with each vertex of the graph
+    num_weights: usize,
+}
+
+impl Header {
+    fn parse(line: &str) -> Self {
+        let mut split_iter = line.split(" ");
+        let num_vertices = split_iter
+            .next()
+            .expect("Graph file header does not contain the number of vertices")
+            .parse()
+            .expect("Failed to parse number of vertices");
+        let num_edges = split_iter
+            .next()
+            .expect("Graph file header does not contain the number of edges")
+            .parse()
+            .expect("Failed to parse number of edges");
+        let fmt = match split_iter.next() {
+            Some(fmt) => Format::new(fmt),
+            None => Format::default(),
+        };
+        // If this parameter is omitted,
+        // then the vertices of the graph are assumed to have a single weight
+        let num_weights = match split_iter.next() {
+            Some(ncon) => ncon.parse().expect("Failed to parse num_weights"),
+            None => 1,
+        };
+        Header {
+            num_vertices,
+            num_edges,
+            fmt,
+            num_weights,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn format_new() {
+        let fmt = Format::new("011");
+        dbg!(&fmt);
+        assert!(!fmt.has_vertex_size);
+        assert!(fmt.has_vertex_weight);
+        assert!(fmt.has_edge_weight);
+    }
+
+    #[should_panic]
+    #[test]
+    fn format_new_invalid1() {
+        let _fmt = Format::new("0111");
+    }
+
+    #[should_panic]
+    #[test]
+    fn format_new_invalid2() {
+        let _fmt = Format::new("012");
+    }
+
+    #[should_panic]
+    #[test]
+    fn format_new_invalid3() {
+        let _fmt = Format::new("01");
+    }
+
+    #[test]
+    fn parse_header_success() {
+        let header = Header::parse("10 34");
+        assert_eq!(header.num_vertices, 10);
+        assert_eq!(header.num_edges, 34);
+        assert_eq!(header.fmt, Format::default());
+        assert_eq!(header.num_weights, 1);
+
+        let header = Header::parse("10 34 011");
+        assert_eq!(header.num_vertices, 10);
+        assert_eq!(header.num_edges, 34);
+        assert_eq!(header.fmt, Format::new("011"));
+        assert_eq!(header.num_weights, 1);
+
+        let header = Header::parse("10 34 011 3");
+        assert_eq!(header.num_vertices, 10);
+        assert_eq!(header.num_edges, 34);
+        assert_eq!(header.fmt, Format::new("011"));
+        assert_eq!(header.num_weights, 3);
+    }
+
+    #[should_panic]
+    #[test]
+    fn parse_header_fail_negative() {
+        let _ = Header::parse("10 -34");
+    }
+
     #[test]
     fn unweighted_graph() {
         // graph in Figure 2.(a)
