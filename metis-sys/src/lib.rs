48475c21e4ac147d32fc4b68138221dc9efefde2
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-10-11 22:19:24 +0900

    Support system feature

--- metis-sys/src/lib.rs
+++ metis-sys/src/lib.rs
@@ -1,6 +1,10 @@
 #![allow(non_camel_case_types)]
 
+#[cfg(feature = "source")]
 extern crate metis_src as _src;
+
+#[cfg_attr(feature = "system", link(name = "metis"))]
+extern "C" {}
 
 use num_derive::*;
 
